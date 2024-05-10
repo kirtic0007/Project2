a36f80f13e7414ae66595a1b5f4ca60af41aa4e7
MANPREETnimmi <160765963+MANPREETnimmi@users.noreply.github.com>
2024-05-10 22:47:38 +0530

    Update 2_darray.c

--- 2_darray.c
+++ 2_darray.c
@@ -1,20 +1,39 @@
-#include<stdio.h>
-int main()
-{
-    int a[2][2];
-    a[0][0]=1;
-    a[0][1]=2;
-    a[1][0]=3;
-    a[1][1]=4;
+#include <stdio.h>
+#include <stdlib.h> 
 
-    int i,j;
-    printf("entered matrix is:\n");
-    for(i=0;i<2;i++)
-    {
-        for(j=0;j<2;j++)
-        {
-            printf("%d\t",a[i][j]);
+int main() {
+    int **a; 
+    int rows = 2, cols = 2;
+    int i, j;
+
+  
+    a = (int **)malloc(rows * sizeof(int *));
+    for (i = 0; i < rows; i++) {
+        a[i] = (int *)malloc(cols * sizeof(int));
+    }
+
+  
+    printf("Enter %dx%d matrix elements:\n", rows, cols);
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    
+    printf("Entered matrix is:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", a[i][j]);
         }
         printf("\n");
     }
+
+   
+    for (i = 0; i < rows; i++) {
+        free(a[i]);
+    }
+    free(a);
+
+    return 0;
 }
